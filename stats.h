b3bb5d9442a80157f231712ca791a1c3e274c1fc
MeenaKumari311 <100028868+MeenaKumari311@users.noreply.github.com>
2022-03-03 23:51:16 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -4,5 +4,6 @@ struct Stats compute_statistics(const float*, int);
 typedef void (*alerter_funcptr)();
 void check_and_alert(float , alerter_funcptr , struct Stats);
 
-extern int ;
-extern int ;
+extern int emailAlertCallCount;
+extern int ledAlertCallCount;
+
