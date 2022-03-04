3113089e7c0bced9ddd6546ed1825ff1513dbf67
MeenaKumari311 <100028868+MeenaKumari311@users.noreply.github.com>
2022-03-04 10:28:01 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -2,8 +2,6 @@
 // struct Stats compute_statistics(const float*, int);
 void emailAlerter(void);
 void ledAlerter(void);
-#include "stats.h"
-
     
 typedef void (*alerter_funcptr)();
 // void check_and_alert(float , alerter_funcptr , struct Stats);
