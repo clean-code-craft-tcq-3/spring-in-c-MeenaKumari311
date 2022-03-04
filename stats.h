399d95501fc852cf90f6f6f28e0b350883b641f7
MeenaKumari311 <100028868+MeenaKumari311@users.noreply.github.com>
2022-03-04 08:25:18 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -1,5 +1,5 @@
 
-struct Stats compute_statistics(const float*, int);
+// struct Stats compute_statistics(const float*, int);
 
 typedef void (*alerter_funcptr)();
 void check_and_alert(float , alerter_funcptr , struct Stats);
