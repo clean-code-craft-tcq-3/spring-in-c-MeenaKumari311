02eb83d3fcfaa228fd367909175a5616eea00b21
MeenaKumari311 <100028868+MeenaKumari311@users.noreply.github.com>
2022-03-05 22:14:05 +0530

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -19,8 +19,9 @@ TEST_CASE("reports average, minimum and maximum") {
 }
 
 TEST_CASE("average is NaN for empty array") {
-    float number_nan=0;
+    float number_nan[]=NAN;
     struct Stats computedStats = compute_statistics(&number_nan, 0);
+    
     //All fields of computedStats (average, max, min) must be
     //NAN (not-a-number), as defined in math.h
     
